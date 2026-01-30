01557c6ac894e7416abb6600220cabcad4c10dcc
kv4lsken <51775139+kv4lsken@users.noreply.github.com>
2026-01-30 15:40:21 +0800

    Update DatabaseHelper.java
    untuk password change
    - Taufiq -

--- app/src/main/java/com/example/buddyapplication/DatabaseHelper.java
+++ app/src/main/java/com/example/buddyapplication/DatabaseHelper.java
@@ -171,4 +171,31 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         return list;
     }
 
+    public boolean checkOldPassword(String username, String oldPassword) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor c = db.rawQuery(
+                "SELECT * FROM users WHERE username=? AND password=?",
+                new String[]{username, oldPassword}
+        );
+
+        boolean exists = c.getCount() > 0;
+        c.close();
+        return exists;
+    }
+
+    public boolean updatePassword(String username, String newPassword) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+        cv.put("password", newPassword);
+
+        int result = db.update(
+                "users",
+                cv,
+                "username=?",
+                new String[]{username}
+        );
+
+        return result > 0;
+    }
+    
 }
